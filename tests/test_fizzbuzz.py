27d735c89f31adb593f9014c03e20042f33764b5
Katz Sakai <sakai@onflow.jp>
2020-08-10 14:41:14 +0900

    テストの名称に仕様が現れるようにテスト名を書き換える
    - そうすると、テストコードがドキュメントとして役立つようになる。
    - インナークラスを利用すると、TODOのツリー構造が表現できる

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -2,19 +2,22 @@ from fizzbuzz import FizzBuzz
 import pytest
 
 
-class FizzBuzzテストクラス:
+class FizzBuzz数列と変換規則を扱うFizzBuzzクラス:
     @pytest.fixture(scope="function")
     def fizzBuzz(self):
         return FizzBuzz()
 
-    def _1を渡すと文字列1を返す_test(self, fizzBuzz):
-        assert "1" == fizzBuzz.convert(1)
+    class convertは数を文字列に変換する:
+        def _1を渡すと文字列1を返す_test(self, fizzBuzz):
+            assert "1" == fizzBuzz.convert(1)
 
-    def _2を渡すと文字列2を返す_test(self, fizzBuzz):
-        assert "2" == fizzBuzz.convert(2)
+        def _2を渡すと文字列2を返す_test(self, fizzBuzz):
+            assert "2" == fizzBuzz.convert(2)
 
-    def _3を渡すと文字列Fizzを返す_test(self, fizzBuzz):
-        assert "Fizz" == fizzBuzz.convert(3)
+    class _3の倍数の時は数の代わりにFizzに変換する:
+        def _3を渡すと文字列Fizzを返す_test(self, fizzBuzz):
+            assert "Fizz" == fizzBuzz.convert(3)
 
-    def _5を渡すと文字列Buzzを返す_test(self, fizzBuzz):
-        assert "Buzz" == fizzBuzz.convert(5)
+    class _5の倍数の時は数の代わりにBuzzに変換する:
+        def _5を渡すと文字列Buzzを返す_test(self, fizzBuzz):
+            assert "Buzz" == fizzBuzz.convert(5)
