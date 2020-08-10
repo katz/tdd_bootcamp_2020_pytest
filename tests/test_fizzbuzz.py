b2050eaa43112b199d22e21095a23f53c9a7652c
Katz Sakai <sakai@onflow.jp>
2020-08-10 14:17:23 +0900

    三角測量するさいは、assertを重ねず、テストメソッドを増やした方が良い
    - assertを重ねていると、失敗したassert以降のテストが実行されない事がある（テストが実行されていない＝正しい動作をしているか保証されない）
    - 失敗しているテストのデバッグがしづらくなる
    - assertが沢山あると、テストでどういう動作を期待しているのかが曖昧になり、テストがドキュメントとして機能しなくなる

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -7,3 +7,9 @@ class FizzBuzzテストクラス:
         fizzBuzz = FizzBuzz()
         # 実行 & 検証
         assert "1" == fizzBuzz.convert(1)
+
+    def _2を渡すと文字列2を返す_test(self):
+        # 準備
+        fizzBuzz = FizzBuzz()
+        # 実行 & 検証
+        assert "2" == fizzBuzz.convert(2)
