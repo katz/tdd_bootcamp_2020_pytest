0de8a6509d1e11702f5c014fe1b66ad644ebcbdb
Katz Sakai <sakai@onflow.jp>
2020-08-10 14:32:48 +0900

    残りのテストコードもリファクタリング

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -10,14 +10,8 @@ class FizzBuzzテストクラス:
     def _1を渡すと文字列1を返す_test(self, fizzBuzz):
         assert "1" == fizzBuzz.convert(1)
 
-    def _2を渡すと文字列2を返す_test(self):
-        # 準備
-        fizzBuzz = FizzBuzz()
-        # 実行 & 検証
+    def _2を渡すと文字列2を返す_test(self, fizzBuzz):
         assert "2" == fizzBuzz.convert(2)
 
-    def _3を渡すと文字列Fizzを返す_test(self):
-        # 準備
-        fizzBuzz = FizzBuzz()
-        # 実行 & 検証
+    def _3を渡すと文字列Fizzを返す_test(self, fizzBuzz):
         assert "Fizz" == fizzBuzz.convert(3)
