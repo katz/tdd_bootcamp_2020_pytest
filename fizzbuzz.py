fe2812353d32495b741cd8388610f2d2c85e2e40
Katz Sakai <sakai@onflow.jp>
2020-08-10 14:46:53 +0900

    3と5両方の倍数の時は数の代わりにFizzBuzzに変換する実装の追加

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 class FizzBuzz:
     def convert(self, num: int) -> str:
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
         if num % 3 == 0:
             return "Fizz"
         if num % 5 == 0:
